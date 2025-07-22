93b854eb4bd66212fe1382fb675121e285a6523b
Nikolay <Nikolay@mail.ru>
2025-07-22 14:26:39 +0300

    Реализовано закрытие модального окна успеха заказа по кнопке

--- src/components/ui/order-details/order-details.tsx
+++ src/components/ui/order-details/order-details.tsx
@@ -4,7 +4,8 @@ import doneImg from '../../../images/done.svg';
 import { OrderDetailsUIProps } from './type';
 
 export const OrderDetailsUI: React.FC<OrderDetailsUIProps> = ({
-  orderNumber
+  orderNumber,
+  onClose
 }) => (
   <>
     <h2 className={`${styles.title} text text_type_digits-large mt-2 mb-4`}>
@@ -15,6 +16,16 @@ export const OrderDetailsUI: React.FC<OrderDetailsUIProps> = ({
       className={styles.img}
       src={doneImg}
       alt='изображение статуса заказа.'
+      onClick={onClose}
+      style={{ cursor: 'pointer' }}
+      role='button'
+      aria-label='Закрыть модальное окно'
+      tabIndex={0}
+      onKeyDown={(e) => {
+        if (e.key === 'Enter' || e.key === ' ') {
+          onClose?.();
+        }
+      }}
     />
     <p className='text text_type_main-default mb-1'>
       Ваш заказ начали готовить
